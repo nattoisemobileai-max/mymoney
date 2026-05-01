d3da37aab4b5db4a83becc4d7e141fe09a2b7284
nattoisemobileai-max <nattoise.mobile.ai@gmail.com>
2026-05-01 10:10:43 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// 這裡我們刪除了對 index.css 和 reportWebVitals 的引用，
-// 因為你的資料夾中目前沒有這些檔案。
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
