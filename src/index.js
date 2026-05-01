205a8187ef645d5579f6d15f19a10e6097124045
nattoisemobileai-max <nattoise.mobile.ai@gmail.com>
2026-05-01 11:55:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // 確保這行存在，對應你剛創建成功的 index.css
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 );
